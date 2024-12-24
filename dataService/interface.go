a70b499be9b56228c9cef027d3794fcc10b5f552
liuxiaoshuai1 <liuxiaoshuai1@360.cn>
2024-12-24 10:57:06 +0800

    fix bug

--- dataService/interface.go
+++ dataService/interface.go
@@ -5,5 +5,5 @@ import "context"
 type DataService interface {
 	CanRun(ctc context.Context) bool
 	Run(ctx context.Context) bool
-	FinishRun(ctx context.Context) (int64 bool)
+	FinishRun(ctx context.Context) (int64, bool)
 }
